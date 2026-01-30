2943a308c1217d8a80888bd391f3c2dc0beaf550
Harsh-Tyg <harshtyg2006@gmail.com>
2026-01-30 10:01:50 +0530

    datatype

---
+++ Data.java
@@ -0,0 +1,11 @@
+public class Data
+{
+    public static void main(String[] args) 
+    {
+       int a= 100;
+       float b= 49.33f;
+       
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
